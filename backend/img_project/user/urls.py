f8f20eb24f3a08a01e4e7d620199a44b66d14ff2
munal777 <munalpoudel3@gmail.com>
2025-05-29 17:08:57 +0545

    setup the api path

--- backend/img_project/user/urls.py
+++ backend/img_project/user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import UserAPIView, LoginAPIView, RegisterAPIView
+from .views import UserAPIView, LoginAPIView, RegisterAPIView, UserProfileAPIView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('user/<int:id>/', UserAPIView.as_view(), name='user'),
+    path('user/profile/<int:id>/', UserProfileAPIView.as_view(), name='userProfile'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('register/', RegisterAPIView.as_view(), name='register'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
