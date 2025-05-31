8693d55723dfb075bd6ce42dc3072b8132a4f93e
munal777 <munalpoudel3@gmail.com>
2025-05-31 19:28:50 +0545

    added the send_otp_to_email func to send otp code

--- backend/img_project/user/utils.py
+++ backend/img_project/user/utils.py
@@ -1,12 +1,23 @@
 from django.core.mail import send_mail
 from django.conf import settings
-
+import random
 
 def welcome_mail(user_mail):
     subject = "Welcome to Project Name"
     message = """
     Your account has been created successfully! 🎉
     """
+    app_mail = settings.EMAIL_HOST_USER
+
+    send_mail(subject, message, app_mail, [user_mail], fail_silently=True)
+
+def send_otp_to_email(user_mail):
+    subject = "OTP Code to Change Password"
+
+    generate_otp = random.randint(100000, 999999)
+
+    message = f"Use this code {generate_otp} to change your password"
+
     app_mail = settings.EMAIL_HOST_USER
 
     send_mail(subject, message, app_mail, [user_mail], fail_silently=True)
