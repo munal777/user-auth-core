fd6c3df542396dbad76557a3463514f4d00562c2
munal777 <munalpoudel3@gmail.com>
2025-06-05 21:59:29 +0545

    created a generate otp method with using secrets module

--- backend/img_project/user/utils.py
+++ backend/img_project/user/utils.py
@@ -1,6 +1,6 @@
 from django.core.mail import send_mail
 from django.conf import settings
-import random
+import secrets
 
 def welcome_mail(user_mail):
     subject = "Welcome to Project Name"
@@ -8,13 +8,12 @@ def welcome_mail(user_mail):
     Your account has been created successfully! 🎉
     """
     app_mail = settings.EMAIL_HOST_USER
-
     send_mail(subject, message, app_mail, [user_mail], fail_silently=True)
+
+
 
 def send_otp_to_email(user_mail):
     subject = "OTP Code to Change Password"
-
-    generate_otp = random.randint(100000, 999999)
 
     message = f"Use this code {generate_otp} to change your password"
 
@@ -22,3 +21,10 @@ def send_otp_to_email(user_mail):
 
     send_mail(subject, message, app_mail, [user_mail], fail_silently=True)
 
+
+
+def generate_otp():
+    
+    otp = ''.join(str(secrets.randbelow(10)) for _ in range(6))
+
+    return otp
