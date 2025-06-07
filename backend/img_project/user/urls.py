bb6385ff58a8cfd2edddb19882ece2d6567da63a
munal777 <munalpoudel3@gmail.com>
2025-06-07 14:22:54 +0545

    restructured the api path

--- backend/img_project/user/urls.py
+++ backend/img_project/user/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import UserAPIView, LoginAPIView, RegisterAPIView, UserProfileAPIView, SendOTPView, ValidateOTPView, ChangePasswordAPIView
+from .views import UserAPIView, LoginAPIView, RegisterAPIView, UserListAPIView, UserDetailsAPIView, UserProfileAPIView, SendOTPView, ValidateOTPView, ChangePasswordAPIView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('user/<int:id>/', UserAPIView.as_view(), name='user'),
+    path('users/', UserListAPIView.as_view(), name='users'),
+    path('user/<int:id>/', UserDetailsAPIView.as_view(), name='user_detail'),
     path('user/profile/<int:id>/', UserProfileAPIView.as_view(), name='userProfile'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('register/', RegisterAPIView.as_view(), name='register'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('send-otp/', SendOTPView.as_view(), name='send_otp'),
     path('verify-otp/', ValidateOTPView.as_view(), name="verify_otp"),
