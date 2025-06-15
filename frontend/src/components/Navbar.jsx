46c837219b2a22eb80ee3163c7a07b9bd1b89875
munal777 <munalpoudel3@gmail.com>
2025-06-15 19:02:37 +0545

    configed the tailwindcss

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+export default function Navbar() {
+
+    return (
+        <>
+        <nav>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Profile</li>
+                    <li>Login</li>
+                    <li>Logout</li>
+                </ul>
+            </div>
+        </nav>
+        </>
+    ) 
+
+}
