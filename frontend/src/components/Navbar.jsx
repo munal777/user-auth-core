b6a44c257eef9fb8265841c56dc15fc843d9ac44
munal777 <munalpoudel3@gmail.com>
2025-06-15 21:36:39 +0545

    created basic navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 
-
 export default function Navbar() {
-
-    return (
-        <>
-        <nav>
-            <div>
-                <ul>
-                    <li>Home</li>
-                    <li>Profile</li>
-                    <li>Login</li>
-                    <li>Logout</li>
-                </ul>
-            </div>
-        </nav>
-        </>
-    ) 
-
+  return (
+    <>
+      <nav className="bg-black text-white font-semibold w-full">
+        <ul className="flex space-x-4 p-4 justify-evenly max-w-5xl mx-auto">
+            <li>Home</li>
+            <li>Profile</li>
+            <li>Login</li>
+        </ul>
+      </nav>
+    </>
+  );
 }
