eccb63843a78017904104028762706bf62cb8740
munal777 <munalpoudel3@gmail.com>
2025-06-23 13:11:46 +0545

    completed the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,37 @@
 import React from "react";
+import { useState } from "react";
+import { X, Menu } from "lucide-react";
 
 export default function Navbar() {
+  const [menu, setOpenMenu] = useState(true);
+
   return (
     <>
-      <nav className="bg-black text-white font-semibold w-full">
-        <ul className="flex space-x-4 p-4 justify-evenly max-w-5xl mx-auto">
-            <li>Home</li>
-            <li>Profile</li>
-            <li>Login</li>
-        </ul>
+      <nav className="flex justify-between bg-black text-white font-semibold md:p-4 p-3.5">
+        <div className="md:mx-12">
+          <p>Project</p>
+        </div>
+
+        <div className="hidden md:flex md:space-x-8">
+          <p>Home</p>
+          <p>Dashboard</p>
+          <p>Login</p>
+        </div>
+
+        <div className="md:hidden" onClick={() => setOpenMenu(!menu)}>
+          {menu ? <Menu /> : <X />}
+        </div>
       </nav>
+
+      {!menu && (
+          <div className="bg-black text-white font-semibold w-1/2">
+            <div className="flex flex-col space-y-2 py-6">
+              <p>Home</p>
+              <p>Dashboard</p>
+              <p>Login</p>
+            </div>
+          </div>
+        )}
     </>
   );
 }
