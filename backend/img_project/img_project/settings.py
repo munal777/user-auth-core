20301df66580ceed36274903779554ce46298958
munal777 <munalpoudel3@gmail.com>
2025-07-16 22:22:41 +0545

    updated the settings file

--- backend/img_project/img_project/settings.py
+++ backend/img_project/img_project/settings.py
@@ -29,7 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'user'
+    'user',
+    'cqis',
 ]
 
 MIDDLEWARE = [
@@ -97,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kathmandu'
 
 USE_I18N = True
 
