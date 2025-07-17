f3ca8dec2e71c53c05acade96988602d7ccf3bf4
munal777 <munalpoudel3@gmail.com>
2025-07-17 15:40:53 +0545

    updated the requirements.txt

--- backend/img_project/cqis/views.py
+++ backend/img_project/cqis/views.py
@@ -1,14 +1,22 @@
 import time
 from django.http import StreamingHttpResponse
-from rest_framework.decorators import api_view
+from django.views import View
+import json
 
-def event_stream():
-    for i in range(10):
-        time.sleep(1)  # simulate delay
-        yield f"data: Message {i}\n\n"
 
-@api_view(['GET'])
-def sse_view(request):
-    response = StreamingHttpResponse(event_stream(), content_type='text/event-stream')
-    response['Cache-Control'] = 'no-cache'
-    return response
+def event_stream(pages):
+    for i, pages in enumerate(pages):
+        time.sleep(5)
+
+        result = {
+            "page": i + 1,
+            "summary": f"Summary of Page {i + 1}",
+            "status": "done"
+        }
+
+        yield f"data: {json.dumps(result)}\n\n"
+
+class StreamView(View):
+
+    def post(self, request):
+        pdf = request.FILES.get('files')
