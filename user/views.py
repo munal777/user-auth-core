3c7b1bf85deb3de74386fee344ce1c83b8fee27d
munal777 <munalpoudel3@gmail.com>
2025-10-12 09:14:24 +0545

    refactor: update the login and register view

--- user/views.py
+++ user/views.py
@@ -1,21 +1,22 @@
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .serializers import LoginSerializer, RegisterSerializer, UserSerializer, UserProfileSerializer, SendOTPSerializer, ValidateOTPSerializer, ChangePasswordSerializer
-from django.contrib.auth import login, get_user_model
-from django.shortcuts import get_object_or_404
-from .models import UserProfile
 from rest_framework.permissions import AllowAny, IsAdminUser
-from .permissions import IsOwnerOrReadOnly
-from .tasks import send_otp
-from .utils import generate_otp
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.shortcuts import get_object_or_404
 from django.core.cache import cache
 
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
-User = get_user_model()
 
+from .serializers import LoginUserSerializer, RegisterUserSerializer, UserSerializer, UserProfileSerializer, SendOTPSerializer, ValidateOTPSerializer, ChangePasswordSerializer
+from .models import UserProfile, User
+from .permissions import IsOwnerOrReadOnly
+from .tasks import send_otp
+from .utils import generate_otp
 
 class UserListAPIView(APIView):
 
@@ -115,58 +116,113 @@ class UserProfileAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-class LoginAPIView(APIView):
+# class LoginAPIView(APIView):
 
-    permission_classes = [AllowAny]    
+#     permission_classes = [AllowAny]    
 
-    @swagger_auto_schema(
-        operation_description="Login user and create a session.",
-        request_body=LoginSerializer,
-        responses={
-            200: openapi.Response("Login successful"),
-            400: openapi.Response("Invalid credentials")
-        },
-        tags=["Auth"]
-    )
-    def post(self, request):
-        serializer = LoginSerializer(data = request.data)
+#     @swagger_auto_schema(
+#         operation_description="Login user and create a session.",
+#         request_body=LoginSerializer,
+#         responses={
+#             200: openapi.Response("Login successful"),
+#             400: openapi.Response("Invalid credentials")
+#         },
+#         tags=["Auth"]
+#     )
+#     def post(self, request):
+#         serializer = LoginSerializer(data = request.data)
 
-        if serializer.is_valid():
-            user = serializer.validated_data['user']
+#         if serializer.is_valid():
+#             user = serializer.validated_data['user']
 
-            #creating the sessions
-            login(request, user)
+#             #creating the sessions
+#             login(request, user)
 
-            return Response({
-                "message": "Login successful",
-                "username": user.username,
-            }, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#             return Response({
+#                 "message": "Login successful",
+#                 "username": user.username,
+#             }, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-class RegisterAPIView(APIView):
+# class RegisterAPIView(APIView):
     
+#     permission_classes = [AllowAny]
+
+#     @swagger_auto_schema(
+#         operation_description="Register a new user.",
+#         request_body=RegisterSerializer,
+#         responses={
+#             201: openapi.Response("User registered successfully"),
+#             400: openapi.Response("Validation error")
+#         },
+#         tags=["Auth"]
+#     )
+#     def post(self, request):
+#         serializer = RegisterSerializer(data = request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({   
+#                 "message": "Register successful",
+#             }, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class RegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterUserSerializer
     permission_classes = [AllowAny]
 
     @swagger_auto_schema(
         operation_description="Register a new user.",
-        request_body=RegisterSerializer,
+        request_body=RegisterUserSerializer,
         responses={
             201: openapi.Response("User registered successfully"),
             400: openapi.Response("Validation error")
         },
         tags=["Auth"]
     )
-    def post(self, request):
-        serializer = RegisterSerializer(data = request.data)
-
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({   
-                "message": "Register successful",
-            }, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+            return api_response(
+                result={
+                    "message": "User created successfully",
+                },
+                is_success=True,
+                status_code=status.HTTP_201_CREATED
+            )
+        return api_response(
+            is_success=False,
+            error_message=serializer.errors,
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
    
+
+
+class LoginView(TokenObtainPairView):
+    serializer_class = LoginUserSerializer
+    permission_classes = [AllowAny]
+
+    @swagger_auto_schema(
+        operation_description="Login user and create a session.",
+        request_body=LoginUserSerializer,
+        responses={
+            200: openapi.Response("Login successful"),
+            400: openapi.Response("Invalid credentials")
+        },
+        tags=["Auth"]
+    )
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        return api_response(
+            result=response.data,
+            is_success=True,
+            status_code=status.HTTP_200_OK
+        )
 
 class SendOTPView(APIView):
 
