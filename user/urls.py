6995a66429038939237c09fc3b9a36836fee4918
munal777 <munalpoudel3@gmail.com>
2025-10-12 09:19:54 +0545

    fix: fix url path config:

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import LoginAPIView, RegisterAPIView, UserListAPIView, UserDetailsAPIView, UserProfileAPIView, SendOTPView, ValidateOTPView, ChangePasswordAPIView
+from .views import LoginView, RegistrationView, UserListAPIView, UserDetailsAPIView, UserProfileAPIView, SendOTPView, ValidateOTPView, ChangePasswordAPIView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('users/', UserListAPIView.as_view(), name='users'),
     path('user/<int:id>/', UserDetailsAPIView.as_view(), name='user_detail'),
     path('user/profile/<int:id>/', UserProfileAPIView.as_view(), name='userProfile'),
-    path('login/', LoginAPIView.as_view(), name='login'),
-    path('register/', RegisterAPIView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegistrationView.as_view(), name='register'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('send-otp/', SendOTPView.as_view(), name='send_otp'),
     path('verify-otp/', ValidateOTPView.as_view(), name="verify_otp"),
