b5a190a19ab114ec7f8adaee30bc343b12b2119e
munal777 <munalpoudel3@gmail.com>
2025-10-14 23:15:24 +0545

    refactor: update the approutes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 
 function AppRoutes() {
     return (
