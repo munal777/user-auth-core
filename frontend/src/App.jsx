0a8c995fc8e2df0f1fa4332bc3df7b9703a1bba6
munal777 <munalpoudel3@gmail.com>
2025-10-14 15:54:08 +0545

    feat: config the routes in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <Router>
+      <AppRoutes/>
+    </Router>
   )
 }
 
---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
+
+function AppRoutes() {
+    return (
+        <>
+        <Routes>
+            <Route path=""></Route>
+            <Route path=""></Route>
+        </Routes>
+        </>
+    )
+}
+
+
+export default AppRoutes;
