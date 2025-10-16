231a909a32622e6291f0c765eb418f178f93e720
munal777 <munalpoudel3@gmail.com>
2025-10-16 11:09:38 +0545

    fix: remove the googlesignin com from app routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,8 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
-// import { GoogleSignIn } from "../services/authServices";
-import GoogleSignIn from "../components/GoogleSignIn";
 
 function AppRoutes() {
     return (
@@ -12,7 +10,6 @@ function AppRoutes() {
             <Route path="/" element={<Home/>}></Route>
             <Route path="sign-in/" element={<SignIn/>}></Route>
             <Route path="sign-up/" element={<SignUp/>}></Route>
-            <Route path="google-auth/" element={<GoogleSignIn/>}></Route>
         </Routes>
         </>
     )
